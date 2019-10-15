f0bd4d13d0e34fe5d193f65cbe8c263c49fdd107
Thomas Kimberly <thomasklfs@gmail.com>
2019-10-15 20:47:55 -0300

    Correção no método de pontuação

--- src/Main.kt
+++ src/Main.kt
@@ -7,7 +7,7 @@ fun main() {
 
     frame.contentPane = MainController().mainPane
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-    frame.setSize(1280, 720)
+    frame.setSize(1300, 900)
     frame.isResizable = true
     frame.setLocationRelativeTo(null)
     frame.isVisible = true
